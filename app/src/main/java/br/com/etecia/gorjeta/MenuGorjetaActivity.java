bf0daaee9cf3cf2fed43fe10ed940a2e43699d5a
GabriiellAugustto <gabriel.gauitarra.05@gmail.com>
2023-10-10 22:10:00 -0300

    Edit2

--- app/src/main/java/br/com/etecia/gorjeta/MenuGorjetaActivity.java
+++ app/src/main/java/br/com/etecia/gorjeta/MenuGorjetaActivity.java
@@ -2,13 +2,47 @@ package br.com.etecia.gorjeta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MenuGorjetaActivity extends AppCompatActivity {
 
+    Button button;
+    EditText Valor;
+    boolean isUpdating = false;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_gorjeta_layout);
+        Valor=findViewById(R.id.Valorcomanda);
+        button=findViewById(R.id.buttoncaucula);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isUpdating){
+                    caucular();
+                }else {
+                    return;
+                };
+
+
+            }
+        });
     }
+    private void caucular (){
+        String Comanda = Valor.getText().toString().trim();
+
+
+
+
+    }
+
+
+
 }
--- app/src/main/java/br/com/etecia/gorjeta/SplahActivity.java
+++ app/src/main/java/br/com/etecia/gorjeta/SplahActivity.java
@@ -2,7 +2,9 @@ package br.com.etecia.gorjeta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplahActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class SplahActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splah_layout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MenuGorjetaActivity.class));
+            }
+        },3000);
+
     }
 }
